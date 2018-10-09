fe95b3d2c3fd9c041822afe9c35ba99d2b38e89e
DeveloperJoseph <joseph.developer.peru@gmail.com>
2018-10-09 18:18:21 -0500

    commitv 1.0
    @JosephWhoami

---
+++ Modulo 1/ejercicio1.py
@@ -0,0 +1,16 @@
+#Entrada de mis variables 'x' con un valor de '5' 
+#& 'y' con un valor de '7'  
+x = 5
+y = 7
+print("El valor de 'x' es: ",x)
+print(type(x))#nos retorna el tipo de variable de x
+print("------------------")
+print("El valor de 'y' es: ",y)
+print(type(y))#nos retorna el tipo de variable de y
+print("------------------")
+print("=========================")
+print("La suma es: ",(x + y))#Salida de mis variables (suma)
+print("La resta es: ",(x - y))#Salida de mis variables (resta)
+print("La multiplicación es: ",(x * y))#Salida de mis variables (multiplicacion)
+print("La división es:",(x / y))#Salida de mis variables (division)
+print("La residuo es:",(x % y))#Salida de mis variables (residuo)
---
+++ Modulo 1/ejercicio2.py
@@ -0,0 +1,14 @@
+print("Enter your name: ")
+x = input()
+print("Hello "+x+", Welcome to Python.")
+print("===============================")
+
+myArrayList = ["Banana","Pineaple","Pear","Watermelon","Orange","Strawberry"] 
+
+print("Frutas en stock: ")
+for name in myArrayList:
+    print(">",name)
+
+print("===============================")
+print("Goodbye,"+x)
+print("===============================")
