7c0c6933f3e9be04f254a04b4c896df5084150f0
DeveloperJoseph <joseph.developer.peru@gmail.com>
2018-10-22 23:57:05 -0500

    commit 3.4.3
    @developerjoseph

--- Modulo 1/ejercicio8.py
+++ Modulo 1/ejercicio8.py
@@ -4,4 +4,8 @@
 #En Python, las tuplas están escritas con corchetes.
 
 thisTuple = ("apple","banana","cherry")#new tuple  
-print(thisTuple)#salida de mi tuple
+print("Mi tuple  collection: "+str(thisTuple))#salida de mi tuple
+
+print("El primer item de mi coleccion tuple es: "+str(thisTuple[0]))
+print("El second item de mi coleccion tuple es: "+str(thisTuple[1]))
+print("El tercer item de mi coleccion tuple es: "+str(thisTuple[2]))
