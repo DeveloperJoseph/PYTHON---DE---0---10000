2a2cbf51ec613d5c2d35149b79a11c17ad7b0f5e
DeveloperJoseph <joseph.developer.peru@gmail.com>
2018-10-22 23:53:21 -0500

    commit 3.4.2
    @developerjoseph

---
+++ Modulo 1/ejercicio8.py
@@ -0,0 +1,7 @@
+
+# TUPLES EN PYTHON:
+#Una tupla es una colección que está ordenada e inmutable .
+#En Python, las tuplas están escritas con corchetes.
+
+thisTuple = ("apple","banana","cherry")#new tuple  
+print(thisTuple)#salida de mi tuple
